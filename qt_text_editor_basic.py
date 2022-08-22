4c4f6730a0fb47fc8be32114b84b539b477ca7a1
jdmalham <110071057+jdmalham@users.noreply.github.com>
2022-08-21 20:55:39 -0700

    Update qt_text_editor_basic.py

--- qt_text_editor_basic.py
+++ qt_text_editor_basic.py
@@ -1,17 +1,39 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
+from PyQt6.QtGui import *
 import sys
 import os
-
-app =  QApplication(sys.argv)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Text Editor')
         
+        self.toolbar = QToolBar()
+        font_select = QFontComboBox(currentFontChanged=self.change_font)
+        
+        self.font_sz_up = QAction('Font up',self)
+        self.font_sz_up.triggered.connect(self.font_up)
+        self.toolbar.addAction(self.font_sz_up)
+
+        self.toolbar.addSeparator()
+
+        self.font_sz_down = QAction('Font down',self)
+        self.font_sz_down.triggered.connect(self.font_down)
+        self.toolbar.addAction(self.font_sz_down)
+
+        self.toolbar.addSeparator()
+        
+        self.toolbar.addWidget(font_select)
+
         self.file_name = ''
+        
         self.text = QTextEdit()
+        
+        self.mfont = QFont()
+        self.font_size = 11
+        self.mfont.setPointSize(self.font_size)
+        self.text.setFont(self.mfont)
 
         self.open_button = QPushButton("Open File")
         self.open_button.clicked.connect(self.open_file)
@@ -21,6 +43,7 @@ class MainWindow(QMainWindow):
         self.run_button.clicked.connect(self.run_file)
         
         master_layout = QVBoxLayout()
+        master_layout.addWidget(self.toolbar)
         master_layout.addWidget(self.text)
         
         button_layout = QHBoxLayout()
@@ -56,21 +79,46 @@ class MainWindow(QMainWindow):
             return
         
         with open(self.file_name, "w") as new_file:
-            new_file.write(str(self.text.toPlainText()))
+            new_file.write(self.text.toPlainText())
 
     def run_file(self):
         self.save_file()
         os.system(f"python -u {self.file_name}")
 
+    def change_font(self, font):
+        self.mfont = QFont(font.key(), self.mfont.pointSize())
+        self.text.setFont(self.mfont)
+
     def save_as(self):
         new_name = QFileDialog.getSaveFileName(self, 'Save file as',
         '/home/josephm/','All files (*.*)')
+        
+        if new_name[0] == '':
+            return
+
         self.file_name = new_name[0]
 
         with open(self.file_name,'w') as file:
             file.write(str(self.text.toPlainText()))
 
-window = MainWindow()
-window.show()
+    def font_up(self):
+        point = self.mfont.pointSize()
+        if point <= 39:
+            self.mfont.setPointSize(point+1)
+            self.text.setFont(self.mfont)
 
-app.exec()
+    def font_down(self):
+        point = self.mfont.pointSize()
+        if point > 1:
+            self.mfont.setPointSize(point-1)
+            self.text.setFont(self.mfont)
+
+
+if __name__ == '__main__':
+
+    app =  QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
