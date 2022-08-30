96f39cf679af3837556a03f2529be1a69f83044d
jdmalham <110071057+jdmalham@users.noreply.github.com>
2022-08-29 19:38:43 -0700

    Add files via upload

--- cat-beanie.py
+++ cat-beanie.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 import sys
 import os
+import subprocess
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,7 +11,6 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Cat Beanie')
 
         self.dir_path = '/home/josephm/Desktop/PyQt/directory'
-
         self.file_name = ''
         
         self.toolbar = QToolBar(movable = False)
@@ -18,21 +18,6 @@ class MainWindow(QMainWindow):
         self.folder_open = QAction('Open Folder',self)
         self.folder_open.triggered.connect(self.open_folder)
         self.toolbar.addAction(self.folder_open)
-        
-        self.font_sz_up = QAction('Font up',self)
-        self.font_sz_up.triggered.connect(self.font_up)
-        self.toolbar.addAction(self.font_sz_up)
-
-        self.toolbar.addSeparator()
-
-        self.font_sz_down = QAction('Font down',self)
-        self.font_sz_down.triggered.connect(self.font_down)
-        self.toolbar.addAction(self.font_sz_down)
-
-        self.toolbar.addSeparator()
-        
-        font_select = QFontComboBox(currentFontChanged=self.change_font)
-        self.toolbar.addWidget(font_select)
 
         self.toolbar.addSeparator()
         
@@ -42,18 +27,19 @@ class MainWindow(QMainWindow):
         self.addToolBar(self.toolbar)
 
         self.text = QTextEdit()
-        
-        self.mfont = QFont()
-        self.font_size = 11
-        self.mfont.setPointSize(self.font_size)
-        self.text.setFont(self.mfont)
 
         self.save_button = QPushButton("Save File")
         self.save_button.clicked.connect(self.save_file)
         self.run_button = QPushButton("Run File")
         self.run_button.clicked.connect(self.run_file)
+
+        self.save_shortcut = QShortcut(QKeySequence('Ctrl+S'),self)
+        self.save_shortcut.activated.connect(self.save_file)
+
+        self.folder_shortcut = QShortcut(QKeySequence('Ctrl+Shift+F'),self)
+        self.folder_shortcut.activated.connect(self.open_folder)
         
-        master_layout = QVBoxLayout()
+        self.master_layout = QVBoxLayout()
 
         self.nav_list = QTreeView()
 
@@ -61,20 +47,29 @@ class MainWindow(QMainWindow):
         self.model.setRootPath(self.dir_path)
        
         self.nav_list.setModel(self.model)
-        
         self.nav_list.setRootIndex(self.model.index(self.dir_path))
         self.nav_list.setRootIndex(self.model.index(self.dir_path))
         
         self.nav_list.clicked.connect(self.nav_item_change)
 
+        self.terminal_output = QTextEdit()
+        self.terminal_output.setProperty("class","output")
+
+        self.output_layout = QVBoxLayout()
+        self.output_layout.addWidget(self.text,stretch=3)
+        self.output_layout.addWidget(self.terminal_output,stretch=2)
+        
+        self.output_container = QWidget()
+        self.output_container.setLayout(self.output_layout)
+
         self.text_layout = QHBoxLayout()
         self.text_layout.addWidget(self.nav_list, stretch = 1)
-        self.text_layout.addWidget(self.text, stretch = 4)
+        self.text_layout.addWidget(self.output_container, stretch = 4)
 
         text_container = QWidget()
         text_container.setLayout(self.text_layout)
 
-        master_layout.addWidget(text_container)
+        self.master_layout.addWidget(text_container)
         
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.save_button)
@@ -83,23 +78,26 @@ class MainWindow(QMainWindow):
         button_container = QWidget()
         button_container.setLayout(button_layout)
         
-        master_layout.addWidget(button_container)
+        self.master_layout.addWidget(button_container)
 
         container = QWidget()
-        container.setLayout(master_layout)
+        container.setLayout(self.master_layout)
 
         self.setCentralWidget(container)
         self.show()
 
     def open_folder(self):
+        try:
         
-        dest = QFileDialog.getExistingDirectoryUrl(self,'Open Folder')
-        src = f'/home/josephm/Desktop/PyQt/directory/{dest.fileName()}'
+            dest = QFileDialog.getExistingDirectoryUrl(self,'Open Folder')
+            src = f'/home/josephm/Desktop/PyQt/directory/{dest.fileName()}'
 
-        if dest.path() == '':
+            if dest.path() == '':
+                pass
+            os.symlink(dest.path(), src)
+
+        except FileNotFoundError:
             pass
-
-        os.symlink(dest.path(), src)
 
     def save_file(self):
         if self.file_name == '':
@@ -111,11 +109,10 @@ class MainWindow(QMainWindow):
 
     def run_file(self):
         self.save_file()
-        os.system(f"python -u {self.file_name}")
-
-    def change_font(self, font):
-        self.mfont = QFont(font.key(), self.mfont.pointSize())
-        self.text.setFont(self.mfont)
+        name = self.file_name.replace(' ', '" "')
+        result = subprocess.check_output(f'python -u {name}',shell = True)
+        
+        self.terminal_output.setText(result.decode('utf-8'))
 
     def save_as(self):
         new_name = QFileDialog.getSaveFileName(self, 'Save file as',
@@ -123,27 +120,15 @@ class MainWindow(QMainWindow):
         
         if new_name[0] == '':
             return
-
         self.file_name = new_name[0]
 
         with open(self.file_name,'w') as file:
             file.write(str(self.text.toPlainText()))
 
-    def font_up(self):
-        point = self.mfont.pointSize()
-        if point <= 39:
-            self.mfont.setPointSize(point+1)
-            self.text.setFont(self.mfont)
-
-    def font_down(self):
-        point = self.mfont.pointSize()
-        if point > 1:
-            self.mfont.setPointSize(point-1)
-            self.text.setFont(self.mfont)
-
     def nav_item_change(self):
         try:
-            self.file_name = self.model.filePath(self.nav_list.selectedIndexes()[0])
+            file_info = self.nav_list.model().fileInfo(self.nav_list.selectedIndexes()[0])
+            self.file_name = file_info.absoluteFilePath()
             self.file_label.setText(f'File path: {self.file_name}')
 
             with open(self.file_name, 'r') as file:
@@ -151,7 +136,6 @@ class MainWindow(QMainWindow):
         except UnicodeDecodeError or IsADirectoryError or ValueError:
             self.text.setText('Error opening file')
             pass
-
 
 if __name__ == '__main__':
     app =  QApplication(sys.argv)
@@ -171,6 +155,11 @@ if __name__ == '__main__':
     }
     QTreeView {
         background-color: "grey";
+    }
+    .output {
+        background-color: "#363232";
+        color: "white";
+        border-color: "grey";
     }
 """)
     app.exec()
